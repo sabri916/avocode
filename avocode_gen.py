437d0fa057be6432c29fc30e078d25daf469ceb4
sabri916 <sbrkrs@gmail.com>
2012-12-09 23:25:48 +0000

    fixed

--- avocode_gen.py
+++ avocode_gen.py
@@ -1,9 +1,19 @@
 import xml.etree.ElementTree as ET
+import sys
 from avocode_util import *
 
+for i in sys.argv:
+    print i
 
-tree = ET.parse('avocode_ui.xml')
+if len(sys.argv)>3:
+    print 'Too many arguments'
+    print 'EXAMPLE: avocode_gen XMLfile outputFile'
+    exit()
+    
+if len(sys.argv)<=1:
+    print 'Specify XML file'
+    exit()
+
+tree = ET.parse(sys.argv[1])
 root = tree.getroot()
-createDocument(root)
-   
-
+createDocument(root,sys.argv[2])
